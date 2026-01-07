3cbd5bca58f27ed824d606d97ec081e275d5abc3
yim-yongwoo <yongwoo1207@naver.com>
2026-01-07 11:00:06 +0900

    rmse 추가

--- loss_function.py
+++ loss_function.py
@@ -1,16 +1,30 @@
+
 """
 title : 손실함수 실습
 생성자 : 임용우
 생성일 : 26.01.06
-최종수정일 : 26.01.06
+최종수정일 : 26.01.07
 """
-#1. mse 구현
+
+# 패키지 import 
 import numpy as np
 
+#1. mse 구현
 def mse(y_t, y_p):
   return np.mean((y_t - y_p)**2)
 
-y_t = np.array([1, 2, 3])
-y_p = np.array([1, 2, 4])
-print(mse(y_t, y_p))
+#2. RMSE 구현
+def rmse(y_t, y_f):
+  return mse(y_t, y_f) ** (1/2)
+
+def main():
+  y_t = np.array([1, 2, 3])
+  y_p = np.array([1, 2, 10])
+  print("mse 실습 결과 :", mse(y_t, y_p))
+  print("RMSE 실습 결과 :", rmse(y_t, y_p))
+  return 0
+
+if __name__ == '__main__':
+  main()
+
 
